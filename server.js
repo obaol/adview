7d1658481dd6a154549905c46e68dea6310269bb
Owen Barnes <owenb@me.com>
2013-04-29 18:36:52 +0000

    randomly select a creative

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+var fs = require('fs');
+var path = require('path');
 var http = require('http');
 var nodeStatic = require('node-static');
 
 var fileServer = new nodeStatic.Server('./public');
+
+var creativeDirectory = path.join(__dirname, 'public', 'creatives');
+var availableCreatives = fs.readdirSync(creativeDirectory);
+
 
 // Start the server
 http.createServer(function (req, res) {
@@ -12,15 +18,18 @@ http.createServer(function (req, res) {
 
   switch (req.url) {
 
-    case '/showBanner':
-      console.log('Serving banner');
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end('<html><body style="margin: 0;"><img src="/creatives/d640460042514336bf577c0de1e6e643.gif"></body></html>');
-      break;
-
     case '/':
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end('<iframe src="/showBanner" width="300" height="250" frameborder="0" />');
+      break;
+
+    case '/showBanner':
+
+      var fileName = availableCreatives[Math.floor(Math.random() * availableCreatives.length)];
+
+      console.log('Serving banner');
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('<html><body style="margin: 0;"><img src="/creatives/' + fileName + '"></body></html>');
       break;
 
     default:
